9d25dff2d731fa3052fdfabf07ef546c94192fef
GlobalRecruits <contact@globalrecruits.net>
2022-10-01 23:18:37 +0100

    Added Content to State

--- global-recruits/src/pages/HomePage.tsx
+++ global-recruits/src/pages/HomePage.tsx
@@ -1,12 +1,22 @@
+import axios, { AxiosResponse } from "axios";
+import { useEffect, useState } from "react";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 
 export default function HomePage() {
+    const [content, setContent] = useState("");
+    useEffect(() => {
+        axios.get('https://api.globalrecruits.net/api/HelloWorld?name=GlobalRecruits')
+            .then((response: AxiosResponse) => setContent(response.data))
+            .catch((error: Error) => console.info("Issue Fetching Data: " + error.message));
+    }, [])
     return (
         <>
             <Header />
             <Hero />
             Hello there!
+            <br />
+            {content}
             <br />
             Hello there!
             <br />
