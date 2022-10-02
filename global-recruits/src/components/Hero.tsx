76352102279dacadde070ea49a78aa87cafa847a
GlobalRecruits <contact@globalrecruits.net>
2022-10-02 20:25:44 +0100

    Hero Border Black - 02/10/2022

--- global-recruits/src/components/Hero.tsx
+++ global-recruits/src/components/Hero.tsx
@@ -5,7 +5,7 @@ export default function Hero() {
     return (
         <section className="gradient h-screen font-custom text-black shadow shadow-2xl">
             <div className="container h-full flex items-center justify-center flex-wrap m-auto">
-                <div className="w-full h-2/3 mx-8 border-b-2 sm:mx-0 sm:w-1/2 lg:w-2/3 sm:h-fit sm:border-r-2 sm:border-b-0 flex flex-col items-center justify-center text-center">
+                <div className="w-full h-2/3 mx-8 border-b-2 sm:mx-0 sm:w-1/2 lg:w-2/3 sm:h-fit sm:border-r-2 sm:border-b-0 border-black flex flex-col items-center justify-center text-center">
                     <img className="w-1/2 sm:w-3/12 my-2" src="logo.png" />
                     <span className="text-4xl font-bold">GLOBAL RECRUITS</span>
                     <div className="text-xl mt-2 mb-4">
