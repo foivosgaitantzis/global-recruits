56aae7bd7a6275d7d3f32276606e771851ac28ab
GlobalRecruits <contact@globalrecruits.net>
2022-10-09 18:18:07 +0100

    Added Black Border on Mobile - 09/10/2022

--- global-recruits/src/components/Header.tsx
+++ global-recruits/src/components/Header.tsx
@@ -6,17 +6,17 @@ export default function Header() {
     const [displayNavMenu, setDisplayNavMenu] = useState(false);
 
     return (
-        <nav className={"absolute w-full top-0 text-black " + ((displayNavMenu) && "gradient")}>
+        <nav className={"absolute w-full top-0 text-black " + ((displayNavMenu) && "gradient border-b border-black")}>
             <div className="py-4 px-4">
                 <div className="flex lg:hidden" onClick={() => setDisplayNavMenu(!displayNavMenu)}>
-                    <button className="focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
+                    <button className="focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out border">
                         <svg className="fill-current h-8 w-8" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                             <title>Menu</title>
                             <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
                         </svg>
                     </button>
                 </div>
-                <div className={"w-full flex-grow lg:flex lg:items-center p-4 lg:p-0 text-center container mx-auto " + (!displayNavMenu && "hidden")}>
+                <div className={"w-full flex-grow lg:flex lg:items-center p-4 lg:p-0 text-center container mx-auto " + (!displayNavMenu && "hidden ")}>
                     <ul className="list-reset lg:flex justify-end flex-1 items-center text-xl lg:text-xl font-bold">
                         {true ?
                             <>
