557854d5a8faca63516f5e2303d5702a20c09726
GlobalRecruits <contact@globalrecruits.net>
2023-01-03 21:07:55 +0000

    Re-Work!

--- global-recruits/src/shared/templates/DashboardTemplate.tsx
+++ global-recruits/src/shared/templates/DashboardTemplate.tsx
@@ -8,7 +8,7 @@ interface DashboardTemplateProperties {
 
 export default function DashboardTemplate(props: DashboardTemplateProperties) {
     return (
-        <div className="h-screen flex items-center flex-wrap font-custom">
+        <div className="h-full min-h-screen w-full flex items-center flex-wrap font-custom">
             <div className="gradient-theme h-full w-1/3 sm:w-1/8 md:w-1/6 border-r fixed overflow-hidden">
                 <div className="py-8 px-4 2xl:px-8 h-full min-h-fit overflow-y-auto overflow-x-hidden">
                     <Sidebar />
