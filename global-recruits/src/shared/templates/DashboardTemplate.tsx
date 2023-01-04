4ff3f12eb4105c3babf2f77d7231f5ac1eea7594
GlobalRecruits <contact@globalrecruits.net>
2023-01-04 22:11:58 +0000

    Fixed... - 04/01/2023

--- global-recruits/src/shared/templates/DashboardTemplate.tsx
+++ global-recruits/src/shared/templates/DashboardTemplate.tsx
@@ -11,10 +11,11 @@ export default function DashboardTemplate(props: DashboardTemplateProperties) {
     const [sidebarOpen, setSidebarOpen] = useState<boolean>(true);
 
     return (
-        <div className="flex font-custom relative">
-            <div className={"transition-all transition-slowest ease inset-0 border-r border-gray-350  gradient-theme bg-fixed fixed " + (sidebarOpen ? "w-1/3 landscape:w-1/6 sm:w-1/8 md:w-1/6" : "w-0")}>
-                <div className="absolute right-[-40px] top-0 h-full bg-white bg-fixed lg:hidden">
-                    <button className="w-10 h-10 gradient-theme-inverse bg-fixed flex justify-center items-center border-b border-r border-t border-gray-350" onClick={() => setSidebarOpen(!sidebarOpen)}>
+        <div className="font-custom h-full min-h-screen relative">
+            <div className={"transition-all transition-slowest ease absolute top-0 bottom-0 min-h-full gradient-theme border-r border-gray-350 "  + (sidebarOpen ? "w-1/3 landscape:w-1/6 sm:w-1/8 md:w-1/6" : "w-0")}></div>
+            <div className={"transition-all bottom-0 top-0 transition-slowest ease fix-ios fixed " + (sidebarOpen ? "w-1/3 landscape:w-1/6 sm:w-1/8 md:w-1/6" : "w-0")}>
+                <div className="absolute right-[-40px] top-0 h-full lg:hidden">
+                    <button className="w-10 h-10 gradient-theme-inverse flex justify-center items-center border-b border-r border-t border-gray-350" onClick={() => setSidebarOpen(!sidebarOpen)}>
                         <GiHamburgerMenu className="w-3/4 h-3/4" />
                     </button>
                 </div>
@@ -25,7 +26,7 @@ export default function DashboardTemplate(props: DashboardTemplateProperties) {
                 </div>
             </div>
             <div className={"transition-all transition-slowest ease h-full ml-auto pl-10 lg:pl-0 " + (sidebarOpen ? "w-2/3 landscape:w-5/6 sm:w-7/8 md:w-5/6" : "w-full")}>
-                <div className="m-4">
+                <div className="p-4">
                     {props.children}
                 </div>
             </div>
