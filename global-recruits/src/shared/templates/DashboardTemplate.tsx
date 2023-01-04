8bceca5c2ce620c213401186f9b82e20e4ddcf8b
GlobalRecruits <contact@globalrecruits.net>
2023-01-04 20:27:54 +0000

    Attempt 3 - 04/01/2023

--- global-recruits/src/shared/templates/DashboardTemplate.tsx
+++ global-recruits/src/shared/templates/DashboardTemplate.tsx
@@ -12,15 +12,15 @@ export default function DashboardTemplate(props: DashboardTemplateProperties) {
 
     return (
         <div className="flex font-custom relative">
-            <div className={"transition-all transition-slowest ease gradient-theme inset-0 border-r border-gray-350 fixed " + (sidebarOpen ? "w-1/3 sm:w-1/8 md:w-1/6" : "w-0")}>
-                <div className="absolute right-[-40px] top-0 h-full bg-white lg:hidden">
-                    <button className="w-10 h-10 gradient-theme-inverse m-auto flex justify-center items-center border-b border-r border-t border-gray-350" onClick={() => setSidebarOpen(!sidebarOpen)}>
+            <div className={"transition-all transition-slowest ease inset-0 border-r border-gray-350  gradient-theme bg-fixed fixed " + (sidebarOpen ? "w-1/3 sm:w-1/8 md:w-1/6" : "w-0")}>
+                <div className="absolute right-[-40px] top-0 h-full bg-white bg-fixed lg:hidden">
+                    <button className="w-10 h-10 gradient-theme-inverse bg-fixed flex justify-center items-center border-b border-r border-t border-gray-350" onClick={() => setSidebarOpen(!sidebarOpen)}>
                         <GiHamburgerMenu className="w-3/4 h-3/4" />
                     </button>
                 </div>
-                <div className="h-full min-h-fit overflow-y-auto overflow-x-hidden">
-                    <div className="h-full min-h-fit py-4 xl:py-8 px-4 2xl:px-8">
-                    <Sidebar />
+                <div className="h-full overflow-y-auto overflow-x-hidden">
+                    <div className="h-full py-4 xl:py-8 px-4 2xl:px-8">
+                        <Sidebar />
                     </div>
                 </div>
             </div>
